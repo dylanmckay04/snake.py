5955fb1349e22a27a1b137caffdd2753037fd81a
Dylan <dylanmckay8939@gmail.com>
2024-07-22 18:24:27 -0400

    Add music & sound effects

--- snake.py
+++ snake.py
@@ -13,6 +13,10 @@ def get_random_position():
     y = randrange(0, WINDOW_HEIGHT - BLOCK_SIZE, BLOCK_SIZE)
     return x, y
 
+# Check if apple spawned inside snake
+def is_apple_in_snake(apple_pos, snake_segments):
+    return apple_pos in snake_segments
+
 # Draw background grid
 def draw_grid():
     for x in range(0, WINDOW_WIDTH, BLOCK_SIZE):
@@ -24,11 +28,11 @@ def draw_grid():
 def display_message(text, size, color, y_offset=0):
     font = pygame.font.Font("fonts/data-latin.ttf", size)
     message = font.render(text, True, color)
-    rect = message.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + y_offset))
-    screen.blit(message, rect)
+    message_rect = message.get_rect(center=(WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + y_offset))
+    screen.blit(message, message_rect)
 
 # Welcome screen
-def welcome_message():
+def welcome_screen():
     while True:
         screen.fill("black")
         display_message("snake.py", 75, "green")
@@ -44,11 +48,13 @@ def welcome_message():
 
 # Game over screen
 def game_over(score):
+    pygame.mixer.music.stop()
+    game_over_sound.play()
+    
     while True:
         screen.fill("black")
         display_message(f"Game Over! Your Score: {score}", 75, "red")
         display_message("Press 'R' to Restart or 'Q' to Quit", 50, "white", 100)
-        
         pygame.display.update()
         
         for event in pygame.event.get():
@@ -64,15 +70,32 @@ def game_over(score):
 
 # Main script
 def main():
-    global screen # Allows draw_grid() to access screen
+    global screen, game_over_sound # Allows access to draw_grid(), display_message(), welcome_screen(), game_over()
     pygame.init()
+    pygame.mixer.init()
+    
+    # Load sound effects
+    background_music = "audio/cyborg-ninja.wav"
+    eat_sound = pygame.mixer.Sound("audio/eat-sound.wav")
+    eat_sound.set_volume(.8)
+    game_over_sound = pygame.mixer.Sound("audio/game-over.wav")
+    game_over_sound.set_volume(.2)
+    
+    # Game window configuration
     screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption("Snake.py")
+    pygame.display.set_caption("snake.py")
     clock = pygame.time.Clock()
-
-    welcome_message()
+    
+    # Show welcome screen
+    welcome_screen()
+    
     while True:
-        # Snake details
+        game_over_sound.stop() # Stop game over sound playing over music at start of new game
+        pygame.mixer.music.load(background_music)
+        pygame.mixer.music.play(-1) # Loop background music
+        pygame.mixer.music.set_volume(.1)
+        
+        # Snake attributes
         snake_surface = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
         snake_surface.fill("green")
         snake_x_pos = 560
@@ -80,11 +103,11 @@ def main():
         snake_speed = BLOCK_SIZE
         direction = None
         
-        # Initialize snake segments
+        # Initialize snake segments & score
         snake_segments = [(snake_x_pos, snake_y_pos)]
         score = 0
         
-        # Apple details
+        # Apple attributes
         apple_surface = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
         apple_surface.fill("red")
         apple_x_pos, apple_y_pos = get_random_position()
@@ -107,6 +130,7 @@ def main():
                     elif (event.key == pygame.K_d or event.key == pygame.K_RIGHT) and direction != "LEFT":
                         direction = "RIGHT"
             
+        
             # Update snake direction
             if direction:
                 # Add new head segment based on direction
@@ -128,10 +152,12 @@ def main():
             
             # Check for collision with apple
             if snake_segments[0] == (apple_x_pos, apple_y_pos):
-                apple_x_pos, apple_y_pos = get_random_position()
-                apple_rect.topleft = (apple_x_pos, apple_y_pos)
-                # Add a new segment to the snake
-                snake_segments.append(snake_segments[-1])
+                while is_apple_in_snake((apple_x_pos, apple_y_pos), snake_segments): # Regenerate apple position if position inside snake
+                    apple_x_pos, apple_y_pos = get_random_position()
+                
+                eat_sound.play()
+                apple_rect.topleft = (apple_x_pos, apple_y_pos) # Move apple to new position
+                snake_segments.append(snake_segments[-1]) # Add a new segment to the snake
                 score += 1
                 
             # Ensure the snake stays within the grid
@@ -142,7 +168,6 @@ def main():
             # Clear screen each frame & draw items
             screen.fill("black")
             draw_grid()
-            
             screen.blit(apple_surface, apple_rect.topleft)
             for segment in snake_segments:
                 screen.blit(snake_surface, segment)
@@ -151,6 +176,7 @@ def main():
             pygame.display.update()
             clock.tick(8)
 
+        # Replay game if 'r' key is pressed
         if game_over(score):
             continue
         
